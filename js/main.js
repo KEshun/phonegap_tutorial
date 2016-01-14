b248f2bf65d3dee202e9ae2db4fb6a33cc00cbe8
Kofi Eshun <KEshun8291@gmail.com>
2016-01-14 16:41:11 -0500

    Push to GitHub.com
    Adding project for first time

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var app = {
+
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
+        });
+
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    }
+};
+
+app.initialize();
